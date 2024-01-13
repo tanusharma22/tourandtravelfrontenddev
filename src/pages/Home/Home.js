0a4441dfb02c8eeb859c41bec0a5334d20e46e58
Tanu Sharma <mohitsharma@Mohits-MacBook-Air.local>
2024-01-13 22:51:25 +0530

    profile drop down

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,57 +1,80 @@
-import { Fragment,useEffect,useState } from "react";
-import { getHotelsByPrice,getHotelsByRoomsAndBeds,
-    getHotelsByPropertyType,
-    getHotelsByRatings,
-    getHotelsByCancelation, } from "../../utils";
-import {Navbar,HotelCard,Categories,SearchStayWithDate,Filter,AuthModal,Alert} from "../../components";
-import "./Home.css"
+import { Fragment, useEffect, useState } from "react";
+import {
+  getHotelsByPrice,
+  getHotelsByRoomsAndBeds,
+  getHotelsByPropertyType,
+  getHotelsByRatings,
+  getHotelsByCancelation,
+} from "../../utils";
+import {
+  Navbar,
+  HotelCard,
+  Categories,
+  SearchStayWithDate,
+  Filter,
+  AuthModal,
+  ProfileDropDown,
+  Alert,
+} from "../../components";
+import "./Home.css";
 import axios from "axios";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {useCategory,useDate,useFilter,useAuth,useAlert} from "../../context";
+import {
+  useCategory,
+  useDate,
+  useFilter,
+  useAuth,
+  useAlert,
+} from "../../context";
 export const Home = () => {
-    const [hotels,setHotels]=useState([]);
-    const [testData,setTestData]=useState([])
-    const [hasMore,setHasMore]=useState(true);
-    const [currentIndex,setCurrenntIndex]=useState(16);
-    const {hotelCategory} =useCategory();
-    const {isSearchModalOpen}=useDate();
-    const {isFilterModalOpen,priceRange,noOfBathrooms,
-        noOfBedrooms,
-        noOfBeds,
-        propertyType,
-        traveloRating,
-        isCancelable}=useFilter()
-    const { isAuthModalOpen} = useAuth();
-    const { alert } = useAlert();
+  const [hotels, setHotels] = useState([]);
+  const [testData, setTestData] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [currentIndex, setCurrenntIndex] = useState(16);
+  const { hotelCategory } = useCategory();
+  const { isSearchModalOpen } = useDate();
+  const {
+    isFilterModalOpen,
+    priceRange,
+    noOfBathrooms,
+    noOfBedrooms,
+    noOfBeds,
+    propertyType,
+    traveloRating,
+    isCancelable,
+  } = useFilter();
+  const { isAuthModalOpen , isDropDownModalOpen} = useAuth();
+  const { alert } = useAlert();
 
-    useEffect(()=>{
-       (async()=>{
-        try{
-            const {data} =await axios.get(
-                `https://tourandtravel.cyclic.app/api/hotels?category=${hotelCategory}`);
-            setTestData(data);
-            setHotels(data ? data.slice(0,16):[]);
-            
-        }
-        catch(err)
-        {
-            console.log(err);
-        }
-       })() 
-    },[hotelCategory]);
-    const fetchMoreData=()=>{
-    if(hotels.length>=testData.length){
-        setHasMore(false)
-        return
-    }setTimeout(()=>{
-        if(hotels && hotels.length>0){
-            setHotels(hotels.concat(testData.slice(currentIndex,currentIndex+16)))
-            setCurrenntIndex(prev=>prev+16)
-        }else{
-            setHotels([])
-        }
-    },1000)
-    };
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          `https://tourandtravel.cyclic.app/api/hotels?category=${hotelCategory}`
+        );
+        setTestData(data);
+        setHotels(data ? data.slice(0, 16) : []);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [hotelCategory]);
+  const fetchMoreData = () => {
+    if (hotels.length >= testData.length) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      if (hotels && hotels.length > 0) {
+        setHotels(
+          hotels.concat(testData.slice(currentIndex, currentIndex + 16))
+        );
+        setCurrenntIndex((prev) => prev + 16);
+      } else {
+        setHotels([]);
+      }
+    }, 1000);
+  };
 
   const filteredHotelsByPrice = getHotelsByPrice(hotels, priceRange);
   const filteredHotelsByBedsAndRooms = getHotelsByRoomsAndBeds(
@@ -75,38 +98,36 @@ export const Home = () => {
     isCancelable
   );
 
+  return (
+    <div className="relative">
+      <Navbar />
+      <Categories />
 
-    
-    return(
-        <div className="relative">
-            <Navbar/>
-            <Categories/>
-            
-                {
-                    hotels &&hotels.length>0 ?(
-                        <InfiniteScroll
-                            dataLength={hotels.length}
-                            next={fetchMoreData}
-                            hasMore={hasMore}
-                            loader={hotels.length >0&& <h3 className="alert-text">Loading....</h3>}
-                            endMessage={<p className="alert-text">You have seen it all!</p>}
-                        >
-                            <main className="main d-flex align-center wrap gap-larger">
-                                {filteredHotelsByCancelation&& filteredHotelsByCancelation.map((hotel)=>(<HotelCard key={hotel._id} hotel={hotel}/>))}
-                            </main>
-                        </InfiniteScroll>
-                    ):(<></>)
-                }
-                {isSearchModalOpen &&<SearchStayWithDate/>}
-                {isFilterModalOpen&&<Filter/>}
-                {isAuthModalOpen && <AuthModal />}
-                {alert.open && <Alert />}
-                
-           
-            
-        </div>
-        
-
-
-    );
-}
+      {hotels && hotels.length > 0 ? (
+        <InfiniteScroll
+          dataLength={hotels.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={
+            hotels.length > 0 && <h3 className="alert-text">Loading....</h3>
+          }
+          endMessage={<p className="alert-text">You have seen it all!</p>}
+        >
+          <main className="main d-flex align-center wrap gap-larger">
+            {filteredHotelsByCancelation &&
+              filteredHotelsByCancelation.map((hotel) => (
+                <HotelCard key={hotel._id} hotel={hotel} />
+              ))}
+          </main>
+        </InfiniteScroll>
+      ) : (
+        <></>
+      )}
+      {isDropDownModalOpen && <ProfileDropDown />}
+      {isSearchModalOpen && <SearchStayWithDate />}
+      {isFilterModalOpen && <Filter />}
+      {isAuthModalOpen && <AuthModal />}
+      {alert.open && <Alert />}
+    </div>
+  );
+};
--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,14 +1,14 @@
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
-import { Navbar, HotelCard,AuthModal,Alert} from "../../components";
-import { useWishlist, useAuth ,useAlert} from "../../context";
+import { Navbar, HotelCard, ProfileDropDown, AuthModal, Alert } from "../../components";
+import { useWishlist, useAuth, useAlert } from "../../context";
 import "./Wishlist.css";
 
 export const Wishlist = () => {
   const { wishlist } = useWishlist();
-  const navigate = useNavigate();
-  const { isAuthModalOpen} = useAuth();
+  const { isAuthModalOpen, isDropDownModalOpen } = useAuth();
   const { alert } = useAlert();
+  const navigate = useNavigate();
 
   const handleClickHereClick = () => {
     navigate("/")
@@ -16,17 +16,17 @@ export const Wishlist = () => {
 
   return (
     <Fragment>
-      <Navbar route="wishlist"/>
+      <Navbar route="wishlist" />
       <h2 className="heading-2 d-flex justify-center">Your Wishlist</h2>
       {
         wishlist.length > 0 ? <section className="wishlist-page d-flex align-center wrap gap-larger">
-        {wishlist &&
-          wishlist.map((hotel) => <HotelCard key={hotel._id} hotel={hotel} />)}
-      </section> : <p className="d-flex justify-center">Wishlist Empty. &nbsp;<span className="click-here" onClick={handleClickHereClick}>Click here </span> &nbsp; to add to wishslit</p>
+          {wishlist &&
+            wishlist.map((hotel) => <HotelCard key={hotel._id} hotel={hotel} />)}
+        </section> : <p className="d-flex justify-center">Wishlist Empty. &nbsp;<span className="click-here" onClick={handleClickHereClick}>Click here </span> &nbsp; to add to wishslit</p>
       }
-    {isAuthModalOpen && <AuthModal />}
-    {alert.open && <Alert />}
+      {isDropDownModalOpen && <ProfileDropDown />}
+      {isAuthModalOpen && <AuthModal />}
+      {alert.open && <Alert />}
     </Fragment>
-    
   );
 };
